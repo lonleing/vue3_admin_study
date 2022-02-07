7428ad4a8bedc112bb4174f6da38f3e0f7e56c57
lonleing <1186545015@qq.com>
2022-02-07 23:01:12 +0800

    mock路由服务

--- mock/router.ts
+++ mock/router.ts
@@ -1,17 +1,17 @@
 import 'reflect-metadata'
 import { RouteMeta } from './type'
 const { ROUTER_MAP, BASE_PATH_MAP } = require('./constant')
-const { apiDir } = require('../src/config/default/mock.config')
+const { apisDir } = require('../src/config/default/mock.config')
 const path = require('path')
 const fs = require('fs')
 
 const addRouter = (router: any) => {
-    function ergodicFile(dir: string, isApisDir=false) {
+    function ergodicFile(dir: string, apisDirName='') {
         fs.readdirSync(dir).forEach((controllerItemPath: string) => {
             const controllerItemPathFull = path.join(dir, controllerItemPath)
             const isDir = fs.lstatSync(controllerItemPathFull).isDirectory()
             if (isDir) {
-                ergodicFile(controllerItemPathFull, apiDir.includes(controllerItemPath))
+                ergodicFile(controllerItemPathFull, apisDir.includes(controllerItemPath) ? controllerItemPath : '')
             } else if (/\.(?:t|j)s$/.test(controllerItemPathFull)) {
                 const classModule = require(controllerItemPathFull)
                 const routeMap: RouteMeta[] | undefined = Reflect.getMetadata(ROUTER_MAP, classModule, 'method')
@@ -20,9 +20,8 @@ const addRouter = (router: any) => {
                     const moduleInstances = new classModule()
                     routeMap.forEach((route) => {
                         const { name, method, path } = route
-                        const newPath = basePath ? (basePath.path + path) : path
-                        console.log(apiDir.indexOf(controllerItemPath))
-                        // newPath = isApisDir ? '/api' : newPath
+                        let newPath = basePath ? `${basePath.path}${path}` : path
+                        newPath = apisDirName ? `/${apisDirName}${newPath}` : newPath
                         router[method](newPath, moduleInstances[name])
                     })
                 }
