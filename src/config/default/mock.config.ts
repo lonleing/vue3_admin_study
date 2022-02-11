d2847495d207062c96e1e19a06b5df4f9235a136
lonleing <1186545015@qq.com>
2022-02-11 21:04:47 +0800

    mock配置

--- src/config/default/mock.config.ts
+++ src/config/default/mock.config.ts
@@ -6,8 +6,12 @@ const host = 'localhost'
 /* 注册api目录 */
 const apisDir = [ 'apis' ]
 
+/* 跨域url配置 */
+const originUrl = 'http://localhost:9999'
+
 module.exports = {
     port,
     host,
-    apisDir
+    apisDir,
+    originUrl
 }
