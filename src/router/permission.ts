7f1ff966d3c6765942eccb5b29d4448acc45fc34
lonleing <1186545015@qq.com>
2022-02-11 21:13:02 +0800

    路由权限

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -1,7 +1,17 @@
 import router from ".";
+import { getState } from '@/store'
+import { UserState } from '@/store/modules/userStore'
 
 router.beforeEach(async (to) => {
-    console.log(to.meta)
+    const token = getState<UserState>('userStore').token
+    if (token) {
+        if (to.path == '/login') {
+            return {name: 'Dashboard', replace: true}
+        }
+        return true
+    }
+    if (to.meta.requiresAuth) {
+        return {name: 'Login', replace: true}
+    }
+    return true
 })
-
-// 登录验证 路由守卫
