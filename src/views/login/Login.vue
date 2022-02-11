f8c2f681a499904dc1292a56b95d6dc454319e5d
lonleing <1186545015@qq.com>
2022-02-11 21:30:06 +0800

    用户登录功能

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,53 +1,62 @@
 <template>
-    <theme-container :background="StyleAttrs.loginBg" :filled="true">
-        <bubble-animation :limit="12">
-            <div class="login_page">
-                <div class="login_header">
-                    <theme-select />
-                </div>
-                <auth></auth>
-            </div>
-        </bubble-animation>
-    </theme-container>
+  <theme-container :background="StyleAttrs.loginBg" :filled="true">
+    <bubble-animation :limit="12">
+      <div class="login_page">
+        <div class="login_header">
+          <theme-select />
+        </div>
+        <auth></auth>
+      </div>
+    </bubble-animation>
+  </theme-container>
 </template>
 
 <script lang="ts">
-import ThemeContainer, { StyleAttrs } from '@/components/container/theme-container/ThemeContainer.vue'
+import ThemeContainer, {
+  StyleAttrs,
+} from '@/components/container/theme-container/ThemeContainer.vue'
 import BubbleAnimation from '@/components/animation/BubbleAnimation.vue'
 import Auth from './components/Auth.vue'
 import ThemeSelect from '@/components/theme-select/ThemeSelect.vue'
+import { onMounted, nextTick } from 'vue'
 export default {
-    components: {
-        ThemeContainer,
-        BubbleAnimation,
-        Auth,
-        ThemeSelect
-    },
-    setup () {
-        return {
-            StyleAttrs,
-        }
+  components: {
+    ThemeContainer,
+    BubbleAnimation,
+    Auth,
+    ThemeSelect,
+  },
+  setup() {
+    onMounted(() => {
+      nextTick(() => {
+        const div = document.querySelector('#first_load_screen') as HTMLElement
+        div.style.display = 'none'
+      })
+    })
+    return {
+      StyleAttrs,
     }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .login_page {
-    height: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  .login_header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    color: white;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    position: relative;
-    .login_header {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        color: white;
-        display: flex;
-        justify-content: flex-end;
-        padding: 10px 20px;
-    }
+    justify-content: flex-end;
+    padding: 10px 20px;
+  }
 }
 </style>
--- src/views/login/components/AuthLogin.vue
+++ src/views/login/components/AuthLogin.vue
@@ -1,105 +1,160 @@
 <template>
-    <div class="login_center_box">
-        <h4>欢迎登陆</h4>
-        <div class="login_centent_box">
-            <login-input label="用户名" v-model="user_name" placeholder="请输入用户名"></login-input>
-            <login-input label="密码" placeholder="请输入密码" type="password" :maxLength="16"></login-input>
-            <div class="login_other_box">
-                <label>
-                    <input type="checkbox" value="" hidden />
-                    <div></div>记住密码
-                </label>
-                <span class="to_forget_password" @click="toForgetPassword">忘记密码</span>
-            </div>
-            <div style="margin-top: 30px">
-                <login-button text="登录" @loginEvent="login"></login-button>
-            </div>
-        </div>
+  <div class="login_center_box">
+    <h4>欢迎登陆</h4>
+    <div class="login_centent_box">
+      <login-input
+        label="用户名"
+        v-model="username"
+        placeholder="请输入用户名"
+        @enter="login"
+      ></login-input>
+      <login-input
+        label="密码"
+        v-model="password"
+        placeholder="请输入密码"
+        type="password"
+        :maxLength="16"
+        @enter="login"
+      ></login-input>
+      <div class="login_other_box">
+        <label>
+          <input type="checkbox" value="" hidden />
+          <div></div>
+          记住密码
+        </label>
+        <span class="to_forget_password" @click="toForgetPassword"
+          >忘记密码</span
+        >
+      </div>
+      <div style="margin-top: 30px">
+        <login-button text="登录" @loginEvent="login"></login-button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, inject, ref } from 'vue'
 import LoginButton from './login-button/LoginButton.vue'
 import LoginInput from './login-input/LoginInput.vue'
+import { post } from '@/nets/request'
+import { Http } from '@/nets/httpApi'
+import { ElMessage } from 'element-plus'
+import { setCommit } from '@/store'
+import router from '@/router'
 export default defineComponent({
-    components: {
-        LoginInput,
-        LoginButton
-    },
-    setup() {
-        const user_name = ref('11111')
-        function login() {
-            console.log(user_name.value)
-        }
-        const toComponent = inject('toComponent') as (component: string) => void
-        const toForgetPassword = () => {
-            toComponent('ForgetPassword')
-        }
-        return {
-            user_name,
-            login,
-            toForgetPassword
-        }
+  components: {
+    LoginInput,
+    LoginButton,
+  },
+  setup() {
+    const username = ref('')
+    const password = ref('')
+
+    let isAdopt = true
+    function checkData() {
+      if (!username.value) {
+        isAdopt = false
+        ElMessage({
+          message: '用户名不能为空',
+          type: 'warning',
+        })
+      } else if (!password.value) {
+        isAdopt = false
+        ElMessage({
+          message: '密码不能为空',
+          type: 'warning',
+        })
+      } else {
+        isAdopt = true
+      }
     }
+
+    function login() {
+      checkData()
+      if (isAdopt) {
+        post(Http.Login, {
+          username: username.value,
+          password: password.value,
+        }).then((data) => {
+          const storeModule = 'userStore'
+          setCommit(storeModule, 'setToken', data.token)
+          setCommit(storeModule, 'setUserInfo', data)
+          router.replace({ name: 'Dashboard' })
+        })
+      }
+    }
+
+    const toComponent = inject('toComponent') as (component: string) => void
+    const toForgetPassword = () => {
+      toComponent('ForgetPassword')
+    }
+
+    return {
+      username,
+      password,
+      login,
+      toForgetPassword,
+    }
+  },
 })
 </script>
 
 <style lang="scss" scoped>
 .login_center_box {
-    width: 300px;
-    border-radius: 10px;
-    border: 1px solid #2d4986;
-    padding: 30px 20px 30px 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    z-index: 2;
-    position: relative;
-    h4 {
-        color: white;
-        font-weight: initial;
-        margin-bottom: 10px;
-    }
-    .login_centent_box {
-        width: 100%;
-        .login_other_box {
-            color: white;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            font-size: 14px;
-            padding-top: 10px;
-            user-select: none;
-            label {
-                display: flex;
-                align-items: center;
-                div {
-                    border-radius: 50%;
-                    width: 15px;
-                    height: 15px;
-                    border: 1px solid white;
-                    margin-right: 5px;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                input:checked + div::before {
-                    content: '';
-                    border-radius: 50%;
-                    width: 72%;
-                    height: 72%;
-                    display: block;
-                    background-color: rgb(68, 118, 255);
-                }
-            }
-            .to_forget_password {
-                cursor: pointer;
-                &:hover {
-                    color: #5fb5ff
-                }
-            }
+  width: 300px;
+  border-radius: 10px;
+  border: 1px solid #2d4986;
+  padding: 30px 20px 30px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+  position: relative;
+  h4 {
+    color: white;
+    font-weight: initial;
+    margin-bottom: 10px;
+  }
+  .login_centent_box {
+    width: 100%;
+    .login_other_box {
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 14px;
+      padding-top: 10px;
+      user-select: none;
+      label {
+        display: flex;
+        align-items: center;
+        div {
+          border-radius: 50%;
+          width: 15px;
+          height: 15px;
+          border: 1px solid white;
+          margin-right: 5px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
+        input:checked + div::before {
+          content: '';
+          border-radius: 50%;
+          width: 72%;
+          height: 72%;
+          display: block;
+          background-color: rgb(68, 118, 255);
+        }
+      }
+      .to_forget_password {
+        cursor: pointer;
+        &:hover {
+          color: #5fb5ff;
+        }
+      }
     }
+  }
 }
 </style>
