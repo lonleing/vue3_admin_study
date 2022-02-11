df0f7890cf6c4fdcbda92d5c727bb91d2cd42a62
lonleing <1186545015@qq.com>
2022-02-11 21:05:43 +0800

    默认配置

--- src/config/default/index.ts
+++ src/config/default/index.ts
@@ -1,1 +1,9 @@
-// 
+export const loadTime = 1000    // 延长首屏加载界面时间
+
+export const aesKey = '1234123412ABCDEF'
+export const iv = 'uis4564568ddf778'
+
+export const auth = {
+    username: 'zmtAdmin',
+    password: 's00pers3cret'
+}
