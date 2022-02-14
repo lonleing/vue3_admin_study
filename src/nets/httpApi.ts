bf63d83f9f6998694eaa269303cceb1e29ce7ec5
lonleing <1186545015@qq.com>
2022-02-14 13:44:56 +0800

    http api

--- src/nets/httpApi.ts
+++ src/nets/httpApi.ts
@@ -1,4 +1,8 @@
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://mock-1600520-1304471224.ap-shanghai.run.tcloudbase.com/apis' : 'http://localhost:3000/apis'
 export const Http = {
-    BaseTestUrl: 'http://localhost:3000/apis',
-    Login: '/login'
+    BaseUrl: baseUrl,
+    Login: '/login',
+    ArticleList: '/article/list',
+    ArticleInfo: '/article/info',
+    Tab: '/tab/getTab'
 } 
